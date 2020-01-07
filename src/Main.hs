66b78d89e85eb2ab756056f3fe78d25c43de9959
Trevor L. McDonell <trevor.mcdonell@gmail.com>
2020-01-07 12:13:12 +0100

    fix runN and add file reading

--- src/Main.hs
+++ src/Main.hs
@@ -11,49 +11,47 @@ module Main where
 
 import Draw
 import Petri
+import Parser
 import GameOfLife
 
 import Data.Array.Accelerate                              as A
 import Data.Array.Accelerate.Data.Bits                    as A
 import Data.Array.Accelerate.Data.Colour.RGBA             as A
-import Data.Array.Accelerate.Interpreter                  as I
+-- import Data.Array.Accelerate.Interpreter                  as I
+import Data.Array.Accelerate.LLVM.Native                  as CPU
 
 import Graphics.Gloss.Accelerate.Data.Picture
 import Graphics.Gloss.Interface.Pure.Simulate
 
 import qualified Data.Bits                                as P
 import qualified Prelude                                  as P
-import Prelude                                            ( IO )
+import Prelude                                            ( IO, (<$>) )
 
 
 main :: IO ()
 main = do
-  let
-      input       = glider1
+  -- input <- bitmapOfGolly <$> parseGolly "samples/c4-orthogonal.rle" :: IO (Matrix Word32)
+  input <- bitmapOfGolly <$> parseGolly "samples/turing_js_r.rle" :: IO (Matrix Word32)
+  -- input <- bitmapOfGolly <$> parseGolly "samples/metapixel-galaxy.rle" :: IO (Matrix Word32)
 
+  let
       Z :. h :. w = arrayShape input
       width       = w * bits input  * P.round factor
       height      = h * P.round factor
-      fps         = 5
-      factor      = 20
+      fps         = 15
+      factor      = 1 --20
 
       bits :: forall sh a. P.FiniteBits a => Array sh a -> Int
       bits _ = P.finiteBitSize (undefined::a)
 
-      disp :: (Bits a, P.FiniteBits a) => Matrix a -> Picture
-      disp x    =
-        let go = runN $ A.map packRGBA8 . rgbaOfBitmap
-         in scale factor factor $ bitmapOfArray (go x) True
-
-      step :: (Num a, Bits a, P.FiniteBits a, FromIntegral Int a)
-           => viewport -> time -> Matrix a -> Matrix a
-      step _ _  = runN gameoflife
+      disp = runN (A.map packRGBA8 . rgbaOfBitmap)
+      step = runN gameoflife
 
   simulate
     (InWindow "Game of Life" (width, height) (10,10))
     black
     fps
     input
-    disp
-    step
+    (\x     -> bitmapOfArray (disp x) False)
+    (\_ _ x -> step x)
 
